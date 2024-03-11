a40c2cd17dfb60ccdd88310d9c78cdbe9d20109e
Dmitrii Volt <dmitriy.volt@gmail.com>
2024-03-11 20:31:26 +0300

    Tasks 1 - 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Task 1
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println();
+
+        // Task 2
+        System.out.println("Task 2");
+        System.out.println("Данные Ф. И. О. сотрудка для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println();
+
+        // Task 3
+        System.out.println("Task 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace("ё", "е"));
+        System.out.println();
     }
 }
